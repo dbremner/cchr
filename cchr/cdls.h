0e4021a0a43b1cb872f554b2d728d1c7f76153f2
sipa <sipa@b869a5d1-d705-0410-b904-cb728ecec70f>
2006-10-28 15:49:17 +0000

    eerste versie cdls.h

--- cchr/cdls.h
+++ cchr/cdls.h
@@ -1,31 +1,83 @@
 #ifndef _dcls_h_
 #define _dcls_h_ 1
 
-#define dcls_declare(type,var) struct { int _s; struct { int _prev,_next; type _data } *_d;} var;
-#define dcls_init(type) { 
+#include <stdint.h>
 
-#define alist_declare(type,var) struct { int _n,_a; type *_d; } var;
-#define alist_init(var) do {(var)._n=0; (var)._a=0; (var)._d=NULL; } while(0);
+typedef uint32_t dcls_pid_t;
+#define DCLS_EMPTY_PID (0xFFFFFFFF)
 
-#define alist_get(var,pos) ((var)._d[(pos)])
-#define alist_ptr(var,pos) ((var)._d+(pos))
-#define alist_new(var,ptr) do { \
-  (ptr)=(var)._d+(var)._n; \
-  (var)._n++; \
-  if ((var)._n>(var)._a) { \
-    (var)._a=(((var)._n*5)/4)+3; \
-    (var)._d=realloc((var)._d,sizeof(*((var)._d))*(var)._a); \
+/**
+ * _d : points to dynamic array
+ * _s : size of the dynamic array
+ * _fe: points to the first (youngest) empty cell (or DCLS_EMPTY_PID if there are no empty cells)
+ * empty cells point to the next empty cell, and have DCLS_EMPTY_PID als _prev
+ * the last empty cell points to DCLS_EMPTY_PID
+ * cell pid=0 is the filled marker, and points to the first/last real filled (or itself if there are none)
+ * things will break if you remove pid 0 from the filled set
+ */
+ 
+/* declare var to be a DCLS of type type */
+#define dcls_declare(type,var) struct { dcls_pid_t _s,_fe; struct { type _data; dcls_pid_t _prev,_next;  } *_d;} var;
+
+/* initialize a DCLS var */
+#define dcls_init(var) do {\
+  (var)._s=1; \
+  (var)._d=malloc(sizeof((var)._d[0])); \
+  (var)._d[0]._next=0; \
+  (var)._d[0]._prev=0; \
+  (var)._fe=DCLS_EMPTY_PID; \
+} while(0);
+
+/* ensure a DCLS var has size positions (including the filled marked) */
+#define dcls_ensure(var,size) do {\
+  dcls_pid_t _ns=(size);\
+  if ((var)._s<_ns) {\
+    (var)._d=realloc((var)._d,sizeof((var)._d[0])*_ns);\
+    while ((var)._s<_ns) { \
+      (var)._d[(var)._s]._prev=DCLS_EMPTY_PID; \
+      (var)._d[(var)._s]._next=(var)._fe; \
+      (var)._fe=(var)._s; \
+      (var)._s++;\
+    } \
   } \
 } while(0);
-#define alist_add(var,val) do { \
-  if ((var)._n+1>(var)._a) { \
-    (var)._a=((((var)._n+1)*5)/4)+3; \
-    (var)._d=realloc((var)._d,sizeof(*((var)._d))*(var)._a); \
-  } \
-  (var)._d[(var)._n]=val; \
-  (var)._n++; \
+
+/* allocate a position in DCLS var, and put it in pid */
+/* the result will neither be in the filled set, nor in the empty set */
+#define dcls_alloc(var,pid) do { \
+  if ((var)._fe==DCLS_EMPTY_PID) dcls_ensure(var,(((var)._s+1)*5)/4+3); \
+  (pid)=(var)._fe; \
+  (var)._fe=(var)._d[(pid)]._next; \
+  (var)._d[(pid)]._prev=DCLS_EMPTY_PID; \
 } while(0);
-#define alist_len(var) ((var)._n)
-#define alist_free(var) do { free((var)._d); (var)._d=NULL; (var)._n=0; (var)._a=0; } while(0);
+
+/* bring a position into the empty set */
+/* @pre: position was allocated (in filled set or not) */
+#define dcls_free(var,pid) do { \
+  if ((var)._d[(pid)]._prev!=DCLS_EMPTY_PID) {\
+    dcls_pid_t _prev=(var)._d[(pid)]._prev; \
+    dcls_pid_t _next=(var)._d[(pid)]._next; \
+    (var)._d[_prev]._next=_next;\
+    (var)._d[_next]._prev=_prev;\
+    (var)._d[(pid)]._prev=DCLS_EMPTY_PID;\
+  }\
+  (var)._d[(pid)]._next=(var)._fe;\
+  (var)._fe=(pid);\
+} while(0);
+
+/* bring a position into the filled set */
+/* @pre: position was allocated (not in filled or empty set) */
+#define dcls_used(var,pid) do { \
+  (var)._d[(pid)]._next=(var)._d[0].next; \
+  (var)._d[(pid)]._prev=0; \
+  (var)._d[(var)._d[0].next]._prev=(pid); \
+  (var)._d[0]._next=(pid); \
+} while(0);
+
+#define dcls_free(var) do { \
+  free((var)._d); \
+  (var)._d=NULL; \
+  (var)._s=0; \
+} while(0);
 
 #endif

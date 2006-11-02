2c55610aa824f574d2bebed58317ef4083b4b901
sipa <sipa@b869a5d1-d705-0410-b904-cb728ecec70f>
2006-11-02 01:20:51 +0000

    voortwerken aan CSM, nog niet af

--- gcd.csm.c
+++ gcd.csm.c
@@ -7,25 +7,32 @@
 #define RULELIST_gcd_1(DEF,SEP) DEF(gcd_1_rule1) SEP DEF(gcd_1_rule2) SEP DEF(gcd_1_rule3)
 
 #define CODELIST_gcd_1_rule1 \
-  CSM_ENSURE(CSM_ARG(arg1)==0) \
-  CSM_KILLSELF \
+  CSM_ENSURE(CSM_ARG(arg1)==0, \
+    CSM_KILLSELF \
+    CSM_END \
+  ) \
 
 #define CODELIST_gcd_1_rule2 \
-  CSM_LOOP_BEGIN(gcd_1,I) \
-    CSM_ENSURE_DIFF(I) \
-    CSM_ENSURE(CSM_ARG(arg1)>=CSM_LARG(I,arg1)) \
-    CSM_KILLSELF \
-    CSM_ADD(gcd_1,CSM_ARG(arg1)-CSM_LARG(I,arg1)) \
-  CSM_LOOP_END(gcd_1,I)
+  CSM_LOOP(gcd_1,I, \
+    CSM_ENSURE_DIFF(I, \
+      CSM_ENSURE(CSM_ARG(arg1)>=CSM_LARG(gcd_1,I,arg1), \
+        CSM_KILLSELF \
+        CSM_ADD(gcd_1,CSM_ARG(arg1)-CSM_LARG(gcd_1,I,arg1)) \
+        CSM_END \
+      ) \
+    ) \
+  )
 
 #define CODELIST_gcd_1_rule3 \
   CSM_MAKE \
-  CSM_LOOP_BEGIN(gcd_1,J) \
-    CSM_ENSURE_DIFF(J) \
-    CSM_ENSURE(CSM_LARG(J,arg1) >= CSM_ARG(arg1)) \
-    CSM_KILL(J) \
-    CSM_ADD(gcd_1,CSM_LARG(J,arg1)-CSM_ARG(arg1)) \
-  CSM_LOOP_END(gcd_1,J)
+  CSM_LOOP(gcd_1,J, \
+    CSM_ENSURE_DIFF(J, \
+      CSM_ENSURE(CSM_LARG(gcd_1,J,arg1) >= CSM_ARG(arg1), \
+        CSM_KILL(J) \
+        CSM_ADD(gcd_1,CSM_LARG(gcd_1,J,arg1)-CSM_ARG(arg1)) \
+      ) \
+    ) \
+  )
 
 CSM_START
 

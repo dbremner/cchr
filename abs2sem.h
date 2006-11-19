8eeba5107928c8413b1d59cc9528cee63ac7150a
sipa <sipa@b869a5d1-d705-0410-b904-cb728ecec70f>
2006-11-19 15:54:29 +0000

    main.c, now for real

---
+++ abs2sem.h
@@ -0,0 +1,8 @@
+#ifndef ABS2SEM_H_
+#define ABS2SEM_H_
+
+void sem_generate_cchr(sem_cchr_t* out,cchr_t* in);
+void sem_cchr_destruct(sem_cchr_t* cchr);
+void sem_cchr_init(sem_cchr_t* cchr);
+
+#endif /*ABS2SEM_H_*/

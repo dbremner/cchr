3131dae550feb4f10d1964adc881e4d1caab28ae
sipa <sipa@b869a5d1-d705-0410-b904-cb728ecec70f>
2007-02-03 17:55:30 +0000

    cuckoo hashing

--- ht_cuckoo_test.c
+++ ht_cuckoo_test.c
@@ -9,42 +9,53 @@ uint32_t hashword(const uint32_t *k,size_t length,uint32_t initval);
 
 
 uint32_t hash1(uint32_t val) {
-  return hashword(&val,1,0x2B7E15162UL);
+  return (uint32_t)hashword(&val,1,0x2B7E15162UL);
 }
 
 uint32_t hash2(uint32_t val) {
-  return hashword(&val,1,0x3243F6A88UL);
+  return (uint32_t)hashword(&val,1,0x3243F6A88UL);
 }
 
 #define uint32_eq(v1,v2) ((v1)==(v2))
 
 ht_cuckoo_code(hash_t,uint32_t,uint32_t,hash1,hash2,uint32_eq)
 
+#define SIZE (1<<17)
+
 int main(void) {
   hash_t ht;
   hash_t_init(&ht);
-  hash_t_double(&ht);
-  hash_t_set(&ht,1,2);
-  assert(*hash_t_get(&ht,1)==2);
-  hash_t_set(&ht,2,3);
-  hash_t_set(&ht,3,5);
-  hash_t_set(&ht,4,7);
-  hash_t_set(&ht,5,11);
-  hash_t_set(&ht,6,13);
-  hash_t_set(&ht,7,17);
-  hash_t_set(&ht,8,19);
-  hash_t_set(&ht,9,23);
-  hash_t_set(&ht,10,29);
-  hash_t_double(&ht);
-  assert(*hash_t_get(&ht,1)==2);
-  assert(*hash_t_get(&ht,2)==3);
-  assert(*hash_t_get(&ht,3)==5);
-  assert(*hash_t_get(&ht,4)==7);
-  assert(*hash_t_get(&ht,5)==11);
-  assert(*hash_t_get(&ht,6)==13);
-  assert(*hash_t_get(&ht,7)==17);
-  assert(*hash_t_get(&ht,8)==19);
-  assert(*hash_t_get(&ht,9)==23);
-  assert(*hash_t_get(&ht,10)==29);
+  uint32_t *key=malloc(sizeof(uint32_t)*SIZE);
+  uint32_t *val=malloc(sizeof(uint32_t)*SIZE);
+  int *del=malloc(sizeof(int)*SIZE);
+  for (int l=0; l<SIZE; l++) {
+  	val[l]=(uint32_t)rand();
+  	key[l]=l+(uint32_t)rand()*SIZE;
+  	del[l]=0;
+  }
+  for (int j=0; j<SIZE; j++) {
+    hash_t_set(&ht,key[j],val[j]);
+    int dl=rand()%SIZE;
+    if (dl<=j) {
+    	hash_t_unset(&ht,key[dl]);
+    	del[dl]=1;
+    }
+    int ow=rand()%SIZE;
+    if (ow<=j) {
+    	val[ow]=rand();
+    	del[ow]=0;
+    	hash_t_set(&ht,key[ow],val[ow]);
+    }
+    /*for (int k=0; k<SIZE; k++) {
+      int mh=(k<=j) && !del[k];
+      assert(hash_t_have(&ht,key[k])==mh);
+      if (mh) assert(*hash_t_get(&ht,key[k])==val[k]);
+    }*/
+  }
+  printf("size=%i used=%i inserts=%i iters=%f\n",ht.size,ht.used,ht.ninsert,(double)ht.niter/ht.ninsert);
+  hash_t_free(&ht);
+  free(key);
+  free(val);
+  free(del);
   return 0;
 }

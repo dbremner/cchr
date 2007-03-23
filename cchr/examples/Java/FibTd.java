e8d85a84e752d36892d43c08a8198e64480885c1
sipa <sipa@b869a5d1-d705-0410-b904-cb728ecec70f>
2007-03-23 14:22:15 +0000

    updated examples, bugfix in csm_cchr:needself

--- cchr/examples/Java/FibTd.java
+++ cchr/examples/Java/FibTd.java
@@ -3,19 +3,17 @@ import runtime.primitive.LogicalInt;
 import runtime.primitive.IntEqualitySolverImpl;
 import runtime.primitive.IntEqualitySolver;
 
-public class Tak {
+public class FibTd {
 
     public static void main(String[] args) throws Exception {
-    if (args.length != 3) printUsage();
+    if (args.length != 1) printUsage();
     else try {
     final int x = Integer.parseInt(args[0]);
-    final int y = Integer.parseInt(args[1]);
-    final int z = Integer.parseInt(args[2]);
 
     IntEqualitySolver solver = new IntEqualitySolverImpl();
-    TakTabHandler handler = new TakTabHandler(solver);
+    FibTdHandler handler = new FibTdHandler(solver);
     LogicalInt logint=new LogicalInt();
-    handler.tellTak(x,y,z,logint);
+    handler.tellFib(x,logint);
     System.out.println(logint.getValue());
 
 
@@ -24,10 +22,10 @@ System.err.println(e.getMessage());
 printUsage();
 }
 }
-    
+
 public final static void printUsage() {
 System.out.println(
-"Usage: java Tak <positive int>"
+"Usage: java FibTd <positive int>"
 );
 }
 }

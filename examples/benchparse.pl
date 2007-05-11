354aa4e4aa279a2051865cde90bcb2d0a26a67e3
sipa <sipa@b869a5d1-d705-0410-b904-cb728ecec70f>
2007-05-11 16:42:52 +0000

    grafiekjes zwart-wit

--- examples/benchparse.pl
+++ examples/benchparse.pl
@@ -3,6 +3,7 @@
 use strict;
 
 my %DATA;
+my %ORDER;
 
 open PIPE,"| gnuplot";
 
@@ -30,6 +31,48 @@ for my $file (@ARGV) {
   close FILE;
 }
 
+for my $bench (keys %DATA) {
+  my $benchdata=$DATA{$bench};
+  my $bsdata=$benchdata->{c};
+  my @SYS=keys %{$benchdata};
+  my %uvals;
+  my $nuvals=0;
+  for my $sys (@SYS) {
+    $uvals{$sys}=1;
+  }
+  for my $size (keys %{$bsdata}) {
+    if ($size ne 'low') {
+      my %vals;
+      my $nvals=0;
+      for my $sys (@SYS) {
+        if (defined ($benchdata->{$sys}->{$size})) {
+	  $vals{$sys}=($benchdata->{$sys}->{$size}->[0]/$benchdata->{$sys}->{$size}->[1]-$benchdata->{$sys}->{low});
+	  $nvals++ if ($vals{$sys}>0.000025);
+	}
+      }
+      if ($nvals == $#SYS+1) {
+        for my $sys (@SYS) {
+	  $uvals{$sys} += log($vals{$sys});
+	}
+	$nuvals++;
+      }
+    }
+  }
+  print "AVG $bench\[$nuvals\] : ";
+  my $loww;
+  for my $sys (@SYS) {
+    my $lw=$uvals{$sys}/$nuvals if ($nuvals>0);
+    $loww=$lw if (!defined($loww) || $lw<$loww);
+  }
+  for my $sys (sort { $uvals{$b}<=>$uvals{$a} } (@SYS)) {
+    if ($nuvals>0) {
+      print "$sys=".exp($uvals{$sys}/$nuvals-$loww)." ";
+      push @{$ORDER{$bench}},$sys;
+    }
+  }
+  print "\n";
+}
+
 print PIPE "set xlabel \"problem size\"\n";
 print PIPE "set ylabel \"time\"\n";
 print PIPE "set logscale xy\n";
@@ -39,7 +82,7 @@ for my $bench (keys %DATA) {
   my $benchdata=$DATA{$bench};
   print PIPE "set title \"Benchmark $bench\"\n";
   my @plots;
-  for my $sys (keys %{$benchdata}) {
+  for my $sys (@{$ORDER{$bench}}) {
     my $data=$benchdata->{$sys};
     my $low=$data->{low};
     open FILE,">bench-$bench-$sys.dat";
@@ -53,11 +96,8 @@ for my $bench (keys %DATA) {
   print PIPE "set output \"bench-$bench.ps\"\n";
   print PIPE "plot ".join(', ',@plots)."\n";
 #  system("ps2pdf14 bench-$bench.ps bench-$bench.pdf");
-  print PIPE "set terminal pdf\n";
+  print PIPE "set terminal pdf monochrome enhanced dashed\n";
   print PIPE "set output \"bench-$bench.pdf\"\n";
-  print PIPE "replot\n";
-  print PIPE "set terminal png small\n";
-  print PIPE "set output \"bench-$bench.png\"\n";
   print PIPE "replot\n";
 }
 close PIPE;

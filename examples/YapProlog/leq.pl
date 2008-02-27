aa8b6d8f8c7ef05959a65fd739fcc5fc6de56a36
sipa <sipa@b869a5d1-d705-0410-b904-cb728ecec70f>
2008-02-27 10:18:39 +0000

    never stored analysis, yap, benchmarks, ram_fib, ...

--- examples/YapProlog/leq.pl
+++ examples/YapProlog/leq.pl
@@ -27,7 +27,7 @@ A: H = E, I = E, R = E, T = E, U = E, W = E, leq(F,E).
 :- module(leq, [test/1,leq/2]).
 :- use_module(library(chr)).
 chr_option(debug,off).
-chr_option(optimize,off).
+chr_option(optimize,full).
 chr_option(check_guard_bindings,off).
 
 
--- examples/benchswi.sh
+++ examples/benchswi.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 cd Prolog
-echo -en "[$1].\ntest($2).\n" | exec pl -T0 -G0 -L0
+
+echo $(echo -en "[$1].\nstatistics(runtime,[C1,_]),statistics(system_time,[S1,_]),test($2),statistics(runtime,[C2,_]),statistics(system_time,[S2,_]),C is C2-C1,R is C2+S2-C1-S1.\n" | pl -O -nodebug -L0 -G0 -T0 -q 2>&1 | fgrep ' = ' | cut -d '=' -f 2 | cut -d , -f 1 | tail -n 2)
+
+
